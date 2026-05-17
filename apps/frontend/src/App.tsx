bb8424b760e70c183cabb497328f4f2694f396a2
benson toh <benson_7667@hotmail.com>
2026-05-17 13:43:33 +0800

    Add Nginx configuration for production deployment and update App header to reflect current environment mode

--- apps/frontend/src/App.tsx
+++ apps/frontend/src/App.tsx
@@ -21,7 +21,7 @@ function App() {
           <img src={viteLogo} className="vite" alt="Vite logo" />
         </div>
         <div>
-          <h1>Get started (v1)</h1>
+          <h1>Get started {import.meta.env.MODE} (v1)</h1>
           <p>
             Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
           </p>
